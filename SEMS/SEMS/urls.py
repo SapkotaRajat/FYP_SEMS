2190a7f7f5e06de5e8e6b535d65f1f793e507595
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-03 01:32:23 +0545

    Update url patterns

--- SEMS/SEMS/urls.py
+++ SEMS/SEMS/urls.py
@@ -31,10 +31,6 @@ urlpatterns = [
     path('', include('ticket_purchase.urls')),
     path('', include('core.urls')),
     path('accounts/', include('allauth.urls')),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SEMS/user_authentication/urls.py
+++ SEMS/user_authentication/urls.py
@@ -1,4 +1,6 @@
+#user_authentication/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,7 +9,18 @@ urlpatterns = [
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_confirmation, name='logout_confirmation'),
     path('logout/confirm/', views.logout_request, name='logout'),
-    path('forgot-password/', views.forgot_password, name='forgot-password'),
     path('profile/', views.profile, name='profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password-reset.html'
+        ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password-reset-done.html'
+        ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password-reset-confirm.html'
+        ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password-reset-complete.html'
+        ), name='password_reset_complete'),
     
 ]

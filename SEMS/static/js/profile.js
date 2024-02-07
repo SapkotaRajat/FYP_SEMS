0883611cb3f6101e1cf99638436b9abbfcf172e6
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-07 21:55:08 +0545

    create and update profile.js
    create and update profile.js file for confirmation modal

---
+++ SEMS/static/js/profile.js
@@ -0,0 +1,30 @@
+// Get the modal
+var modal = document.getElementById("confirmModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("confirmModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//when the user clicks the cancel button, close the modal
+document.getElementById("cancelBtn").addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// Handle confirmation button click
+document.getElementById("confirmBtn").addEventListener("click", function () {
+    document.getElementById("editProfileForm").submit();
+});

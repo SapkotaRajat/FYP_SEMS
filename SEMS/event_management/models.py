d5a75e45efbf771e95ad336dfcbe96d01c1f6b4f
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-10 03:06:40 +0545

    Update Event management Model

--- SEMS/event_management/models.py
+++ SEMS/event_management/models.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -14,25 +14,25 @@ class Event(models.Model):
     time = models.TimeField()
     location = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 class Ticket(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     is_paid = models.BooleanField(default=False)
 
 class Attendee(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class StaffAssignment(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    staff = models.ManyToManyField(User)
+    staff = models.ManyToManyField(settings.AUTH_USER_MODEL)
     role = models.CharField(max_length=100)
-    assigned_by = models.ForeignKey(User, related_name='assigned_by', on_delete=models.CASCADE)
+    assigned_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='assigned_by', on_delete=models.CASCADE)
     assigned_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

c5b1ffe378ca9f94a090cc66a1231b2bdabee4ae
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-13 23:41:24 +0545

    Add some content on homepage

--- SEMS/core/views.py
+++ SEMS/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from event_management.models import Category, Event
+from django.utils import timezone
 
 def index(request):
-    return render(request, 'index.html', {})
+    latest_event = Event.objects.filter(date__gte=timezone.now()).order_by('date').first()
+    upcoming_events = Event.objects.filter(date__gte=timezone.now()).order_by('date')[:6]
+    category = Category.objects.all()
+    return render(request, 'index.html', {'latest_event': latest_event, 'category': category, 'upcoming_events': upcoming_events})
 
 def custom_404(request, exception=None):
     return render(request, 'error.html', status=404)

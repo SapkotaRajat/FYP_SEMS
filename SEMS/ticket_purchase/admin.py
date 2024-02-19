c0281fd5b24d0859e5f591f99b22cedd1f84345b
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-20 00:18:50 +0545

    Register  Ticket model on admin site

--- SEMS/ticket_purchase/admin.py
+++ SEMS/ticket_purchase/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Ticket
 # Register your models here.
+
+
+class TicketAdmin(admin.ModelAdmin):   
+    model = Ticket
+    list_display = ['event', 'price', 'available_quantity', 'ticket_date']
+    search_fields = ['event', 'price', 'available_quantity', 'ticket_date']
+    ordering = ['event']
+    
+    fieldsets = (
+        (None, {'fields': ('event', 'price', 'available_quantity', 'ticket_date')}),
+        ('Ticket Description', {'fields': ('ticket_description',)}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('event', 'ticket_price', 'available_quantity', 'ticket_date', 'ticket_description'),
+        }),
+    )
+    
+    
+admin.site.register(Ticket, TicketAdmin)
+

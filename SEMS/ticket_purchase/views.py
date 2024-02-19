9ebd98b1ff7718cb3977ecc38a9cba77fbda7c7f
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-20 00:17:34 +0545

    Add paypal buttons

--- SEMS/ticket_purchase/views.py
+++ SEMS/ticket_purchase/views.py
@@ -1,51 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import BuyTicketsForm
-from .models import Ticket, Payment
+from .models import Ticket 
+from event_management.models import Event
 
 # Create your views here.
 def ticket_purchase(request):
-    return render(request, 'tickets.html' )
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets.html' , {'tickets': tickets})
 
 @login_required
 def buy_tickets(request, event_name):
-    if request.method == 'POST':
-        ticket_purchase_form = BuyTicketsForm(request.POST, request.FILES)
-        if ticket_purchase_form.is_valid():
-            # Extract form data
-            ticket_quantity = ticket_purchase_form.cleaned_data['ticket_quantity']
-            payment_method = ticket_purchase_form.cleaned_data['payment_method']
-            payment_amount = ticket_purchase_form.cleaned_data['payment_amount']
-            card_holder_name = ticket_purchase_form.cleaned_data['card_holder_name']
-            card_number = ticket_purchase_form.cleaned_data['card_number']
-            expiry_month = ticket_purchase_form.cleaned_data['expiry_month']
-            expiry_year = ticket_purchase_form.cleaned_data['expiry_year']
-            cvv = ticket_purchase_form.cleaned_data['cvv']
-            qr_code_image = ticket_purchase_form.cleaned_data['qr_code_image']
-            
-            # Save ticket details
-            ticket = Ticket.objects.get(event_name=event_name)
-            
-            # Save payment details
-            payment = Payment.objects.create(
-                ticket=ticket,
-                payment_method=payment_method,
-                payment_amount=payment_amount,
-                card_holder_name=card_holder_name,
-                card_number=card_number,
-                expiry_month=expiry_month,
-                expiry_year=expiry_year,
-                cvv=cvv,
-                qr_code_image=qr_code_image,
-                # Add other payment details here
-            )
-            
-            # Redirect to the ticket purchase success page
-            return redirect('ticket-purchase-success')
-    else:
-        ticket_purchase_form = BuyTicketsForm()
-    
-    return render(request, 'buy-tickets.html', {'ticket_purchase_form': ticket_purchase_form, 'event_name': event_name})
+    event = Event.objects.get(title=event_name)    
+    return render(request, 'buy-tickets.html', { 'event': event})
+
 @login_required
 def ticket_purchase_success(request):
     return render(request, 'ticket-purchase-success.html')

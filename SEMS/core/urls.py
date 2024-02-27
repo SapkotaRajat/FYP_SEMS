eee9a920babe3ab089559b3837269c7079f91f66
Rajat Sapkota <sapkotarajat59@gmail.com>
2024-02-27 22:30:36 +0545

    Add preliminary pages

--- SEMS/core/urls.py
+++ SEMS/core/urls.py
@@ -9,4 +9,14 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('plan-your-visit/',views.plan_your_visit,name='plan-your-visit'),
     path('about-us/',views.about,name='about-us'),
+    # <!-- menu for policies, specificaions, venue booking, employment, and so on -->
+    path('policies/',views.policies,name='policies'),
+    path('specifications/',views.specifications,name='specifications'),
+    path('employment/',views.employment,name='employment'),
+    path('venue-booking/',views.venue_booking,name='venue-booking'),
+    path('contact-us/',views.contact_us,name='contact-us'),
+    path('faq/',views.faq,name='faq'),
+    path('terms-and-conditions/',views.terms_and_conditions,name='terms-and-conditions'),
+    path('privacy-policy/',views.privacy_policy,name='privacy-policy'),
+    path('refund-policy/',views.refund_policy,name='refund-policy'),
 ]
